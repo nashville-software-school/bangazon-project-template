a2eeb766415b4e5156590b89b7a956819ea23772
hannahhall <hannahkhall13@gmail.com>
2021-12-02 11:45:38 -0600

    add many-many

--- bangazon_api/models/store.py
+++ bangazon_api/models/store.py
@@ -6,6 +6,7 @@ class Store(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     is_active = models.BooleanField(default=True)
+    favorites = models.ManyToManyField(User, through='Favorite', related_name='favorites')
 
     def __str__(self):
         return self.name
