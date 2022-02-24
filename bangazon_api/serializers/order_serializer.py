a24729a3a812c68213d7403e2fcfc7dad8f04a8a
hannahhall <hannahkhall13@gmail.com>
2022-02-24 13:46:56 -0600

    order serializer

--- bangazon_api/serializers/order_serializer.py
+++ bangazon_api/serializers/order_serializer.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from bangazon_api.models import Order
 from bangazon_api.models.payment_type import PaymentType
-
+from .payment_type_serializer import PaymentTypeSerializer
 
 class OrderSerializer(serializers.ModelSerializer):
+    payment_type = PaymentTypeSerializer()
     class Meta:
         model = Order
-        fields = ('id', 'products', 'created_on', 'completed_on', 'total')
+        fields = ('id', 'products', 'created_on', 'completed_on', 'total', 'payment_type')
         depth = 1
 
 class UpdateOrderSerializer(serializers.ModelSerializer):
